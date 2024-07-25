fa4ad5c1c4c139f5f22b9ba84799a515b3eb0761
José Leonardo <joseleosous@gmail.com>
2024-07-25 00:37:41 -0300

    Adicionando endpoints que faltavam

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -17,10 +17,45 @@ namespace ToDoList.Controllers
             _taskInterface = taskInterface;
         }
 
+        [HttpGet("GetAllTasks")]
+        public async Task<ActionResult<ResponseModel<List<TaskItem>>>> GetAllTasks()
+        {
+            var tasks = await _taskInterface.GetAllTasks();
+            return Ok(tasks);
+        }
+
+        [HttpGet("GetTaskById/{id}")]
+        public async Task<ActionResult<ResponseModel<TaskItem>>> GetTaskById(int id)
+        {
+            var task = await _taskInterface.GetTaskById(id);
+            return Ok(task);
+        }
+
+        [HttpGet("GetTaskByTitle/{title}")]
+        public async Task<ActionResult<ResponseModel<TaskItem>>> GetTaskByTitle(string title)
+        {
+            var task = await _taskInterface.GetTaskByTitle(title);
+            return Ok(task);
+        }
+
         [HttpPost("CreateTask")]
         public async Task<ActionResult<ResponseModel<List<TaskItem>>>> CreateTask(CreateTaskDto task)
         {
             var tasks = await _taskInterface.CreateTask(task);
+            return Ok(tasks);
+        }
+
+        [HttpPut("UpdateTask")]
+        public async Task<ActionResult<ResponseModel<List<TaskItem>>>> UpdateTask(UpdateTaskDto task)
+        {
+            var tasks = await _taskInterface.UpdateTask(task);
+            return Ok(tasks);
+        }
+
+        [HttpDelete("DeleteTask")]
+        public async Task<ActionResult<ResponseModel<List<TaskItem>>>> DeleteTask(int id)
+        {
+            var tasks = await _taskInterface.DeleteTask(id);
             return Ok(tasks);
         }
     }
